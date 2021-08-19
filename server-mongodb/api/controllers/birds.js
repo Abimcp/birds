88347bf5a965e58521f25f90ffaa08d7cdc5c0fa
Abigail McLeod-Peck <abigail.m.peck@gmail.com>
2021-08-19 15:01:19 +0100

    birds changes

--- server-mongodb/api/controllers/birds.js
+++ server-mongodb/api/controllers/birds.js
@@ -13,7 +13,46 @@ router.get("/", async (req, res) => {
   }
 });
 
+// birds show route
+router.get("/:id", async (req, res) => {
+  try {
+    const bird = await Bird.findById(req.params.id);
+    res.json(bird);
+  } catch (err) {
+    res.status(404).json({ err });
+  }
+});
 
+// Create birds route
+router.post("/", async (req, res) => {
+  try {
+    const bird = await Bird.create(req.body.name, req.body.age);
+    res.json(bird);
+  } catch (err) {
+    res.status(404).json({ err });
+  }
+});
 
+// birds update route
+router.patch("/:id", async (req, res) => {
+  try {
+    const bird = await bird.findById(req.params.id);
+    const updatedBird = await bird.update();
+    res.json({ bird: updatedBird });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+// delete bird route
+router.delete("/:id", async (req, res) => {
+  try {
+    const bird = await Bird.findById(req.params.id);
+    await bird.destroy();
+    res.status(204).json("Bird deleted");
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
 
 module.exports = router;
